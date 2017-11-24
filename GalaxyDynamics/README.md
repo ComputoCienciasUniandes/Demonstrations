8973a22227f750dab151424473e97bc2c0b067d1
Juan Barbosa <js.barbosa10@uniandes.edu.co>
2017-11-23 19:40:18 -0500

    sPendulum, astrohut, rippletank

--- GalaxyDynamics/README.md
+++
@@ -1,6 +0,0 @@
-### Barnes - Hut implementation
-
-To install:
-`python setup.py install`
-
-From [astro-hut](https://github.com/jsbarbosa/astro-hut)
--- GalaxyDynamics/astrohut/bruteforce.h
+++
@@ -1,4 +0,0 @@
-void force(box *tree);
-void calculateEnergy(void);
-void print_status(const char *dir, int contador);
-void solver(DOUBLE t0, DOUBLE tmax, DOUBLE dt, const char *dir);
